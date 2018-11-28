1c7ddb985667212886986489d7d43cd22c6aecbf
基督徒.羅小雪 <snowray712000@gmail.com>
2018-11-29 02:37:49 +0800

    example - generate map in div

---
+++ generateMap.js
@@ -0,0 +1,11 @@
+function generateMap(idMapDiv){
+    var map = L.map(idMapDiv).setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.satellite', //mapbox.satellite  apbox.streets
+    accessToken: 'your.mapbox.access.token' 
+    }).addTo(map);
+
+    return map;
+}
