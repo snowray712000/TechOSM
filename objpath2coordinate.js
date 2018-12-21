27398b82d2b7ba632b3ee8e3769ef2786f3ff563
基督徒.羅小雪 <snowray712000@gmail.com>
2018-12-21 22:39:23 +0800

    Update objpath2coordinate.js
    可能是多個空白,從?改為*

--- objpath2coordinate.js
+++ objpath2coordinate.js
@@ -3,7 +3,7 @@ function objpath2coordinate(objpathStr)
     // 這是一個 float reg: /^-?\d+(\.\d+)?$/
     // 201812: 51.15023,空白51.1251 ...多了空白的可能 
     var re = []
-    var reg = /(-?\d+(\.\d+)?), ?(-?\d+(\.\d+)?)/g
+    var reg = /(-?\d+(\.\d+)?), *(-?\d+(\.\d+)?)/g
     var tmp
     while ((tmp = reg.exec(objpathStr))!==null)
     {
